f2198d43cf360a7d0f3c50e0901f0d3fd75ffe55
Chengqunniu <chengqunniu@gmail.com>
2022-09-22 11:05:42 +1000

    create a function to check password and run main program

--- src/main.py
+++ src/main.py
@@ -1,9 +1,26 @@
+import json
 from art import text2art
 from class_for_text import text, display_message
 from display_options import welcome_page
 
+def check_password():
+    '''Check password
+    '''
+    password = input('Enter your password:')
+    with open('Password.json', 'rb') as file:
+        customer_password = json.load(file)
+    correct = True
+    while correct:
+        if password == customer_password:
+            correct = False
+        else:
+            password = input('Enter your password again:')
+
+    return correct
 
 def main():
+    '''Main program
+    '''
     welcome_messages = text2art('SmoonyPaws', font='dancingfont')
     message = display_message(welcome_messages)
     press_enter = text('Press enter to continue...')
@@ -14,4 +31,5 @@ def main():
     welcome_page()
 
 if __name__ == '__main__':
+    check_password()
     main()
